1bc22134aa0b349c228cc2f9c10812a219fea5ac
LoicVanlaecke <loicl9@hotmail.com>
2017-01-03 17:57:26 +0100

    next

--- routes/todo.js
+++ routes/todo.js
@@ -25,25 +25,3 @@ module.exports = {
         })
     }
 }
-create: function (req, res) {
-        var todoContent = req.body.content;
-        // create todo
-        Todo.create({
-            content: todoContent
-        }, function (err, todo) {
-            if (err) res.render('error', {
-                    error: 'Error creating your todo :('
-                })
-                // reload collection
-            res.redirect('/todos');
-        });
-    }
-    , destroy: function (req, res) {
-        var id = req.params.id;
-        Todo.findByIdAndRemove(id, function (err, todo) {
-            if (err) res.render('error', {
-                error: 'Error deleting todo'
-            });
-            res.redirect('/todos');
-        });
-    },
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var port = 3000;
 var app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+// telling Express to serve static objects from /public 
+app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 var main = require('./routes/main');
 var todo = require('./routes/todo');
@@ -11,10 +26,7 @@ todoRouter.get('/', todo.all);
 todoRouter.post('/create', todo.create);
 todoRouter.post('/destroy/:id', todo.destroy);
 todoRouter.post('/edit/:id', todo.edit);
-require('./config/models/Todo');
-mongoose.connect('192.168.99.100:27017', function () {
-    console.log('connected to database!')
-});
+// Start server
 app.listen(port, function () {
     console.log('Server listening on port ' + port)
 });
